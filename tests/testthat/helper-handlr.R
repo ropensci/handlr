4a37935da6bde3ffede172bbf5025ab1d1cbf64d
Scott Chamberlain <myrmecocystus@gmail.com>
2018-12-29 11:02:07 -0800

    different skip if internet down setup

--- tests/testthat/helper-handlr.R
+++ tests/testthat/helper-handlr.R
@@ -1,4 +1,15 @@
+# skip_if_net_down <- function() {
+#   testthat::skip_if(!crul::ok("https://httpbin.org/get"),
+#     "internet down")
+# }
+
+has_internet <- function() {
+  !is.null(suppressWarnings(utils::nsl("www.google.com")))
+}
+
 skip_if_net_down <- function() {
-  testthat::skip_if(!crul::ok("https://httpbin.org/get"),
-    "internet down")
+  if (has_internet()) {
+    return()
+  }
+  testthat::skip("no internet")
 }
